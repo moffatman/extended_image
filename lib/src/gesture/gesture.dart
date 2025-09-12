570bcb004825155bad9703f78a0b832c19327057
Callum Moffat <smartercallum@gmail.com>
2025-09-11 21:56:44 -0400

    Improve gesture widget with GestureWidgetLayout
    - Completely avoid requirement for known width/height
    - Implement layoutInsets

--- lib/src/gesture/gesture.dart
+++ lib/src/gesture/gesture.dart
@@ -7,6 +7,7 @@ import 'package:extended_image/src/image/raw_image.dart';
 import 'package:extended_image/src/utils.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import '../typedef.dart';
 import 'page_view/gesture_page_view.dart';
@@ -59,20 +60,18 @@ class ExtendedImageGesture extends StatefulWidget {
 class ExtendedImageGestureWidget extends StatefulWidget {
   const ExtendedImageGestureWidget({
     required this.child,
-    required this.width,
-    required this.height,
     this.heroBuilderForSlidingPage,
     this.initGestureConfigHandler,
-    this.fit,
+    this.fit = BoxFit.contain,
+    this.layoutInsets = EdgeInsets.zero,
     CanScaleImage? canScaleImage,
     super.key
   }) : canScaleImage = canScaleImage ?? _defaultCanScaleImage;
   final Widget child;
   final HeroBuilderForSlidingPage? heroBuilderForSlidingPage;
   final GestureConfig Function()? initGestureConfigHandler;
-  final int width;
-  final int height;
-  final BoxFit? fit;
+  final BoxFit fit;
+  final EdgeInsets layoutInsets;
   final CanScaleImage canScaleImage;
   @override
   ExtendedImageGestureStateWidget createState() => ExtendedImageGestureStateWidget();
@@ -110,9 +109,6 @@ abstract class ExtendedImageGestureStateBase<T extends StatefulWidget> extends S
   Object? get _currentImageKey;
   GestureConfig? _makeGestureConfig();
   VoidCallback? _makeOnDoubleTap();
-  int get width;
-  int get height;
-  BoxFit? get fit;
   HeroBuilderForSlidingPage? get _heroBuilderForSlidingPage;
   bool _canScaleImage(GestureDetails? details);
   Widget _buildImpl();
@@ -517,21 +513,13 @@ abstract class ExtendedImageGestureStateBase<T extends StatefulWidget> extends S
 
   Widget wrapGestureWidget(
     Widget child, {
-    double? imageWidth,
-    double? imageHeight,
-    BoxFit? imageFit,
-    Rect? rect,
-    bool copy = false,
+    BoxFit fit = BoxFit.contain,
+    EdgeInsets layoutInsets = EdgeInsets.zero,
   }) {
-    child = CustomSingleChildLayout(
-      delegate: GestureWidgetDelegateFromState(
-        this,
-        imageWidth: imageWidth,
-        imageHeight: imageHeight,
-        imageFit: imageFit,
-        rect: rect,
-        copy: copy,
-      ),
+    child = GestureWidgetLayout(
+      fit: fit,
+      layoutInsets: layoutInsets,
+      gestureDetails: gestureDetails,
       child: child,
     );
 
@@ -568,13 +556,6 @@ class ExtendedImageGestureStateImage extends ExtendedImageGestureStateBase<Exten
   @override
   GestureConfig? _makeGestureConfig() =>
     widget.extendedImageState.imageWidget.initGestureConfigHandler?.call(widget.extendedImageState);
-
-  @override
-  int get width => widget.extendedImageState.extendedImageInfo!.image.width;
-  @override
-  int get height => widget.extendedImageState.extendedImageInfo!.image.height;
-  @override
-  BoxFit? get fit => widget.extendedImageState.imageWidget.fit;
 
   @override
   HeroBuilderForSlidingPage? get _heroBuilderForSlidingPage =>
@@ -642,13 +623,6 @@ class ExtendedImageGestureStateWidget extends ExtendedImageGestureStateBase<Exte
   GestureConfig? _makeGestureConfig() => widget.initGestureConfigHandler?.call();
 
   @override
-  int get width => widget.width;
-  @override
-  int get height => widget.height;
-  @override
-  BoxFit? get fit => widget.fit;
-
-  @override
   HeroBuilderForSlidingPage? get _heroBuilderForSlidingPage =>
       widget.heroBuilderForSlidingPage;
 
@@ -657,97 +631,272 @@ class ExtendedImageGestureStateWidget extends ExtendedImageGestureStateBase<Exte
 
   @override
   Widget _buildImpl() {
-    return wrapGestureWidget(widget.child, imageWidth: widget.width.toDouble(), imageHeight: widget.height.toDouble());
+    return wrapGestureWidget(widget.child, fit: widget.fit, layoutInsets: widget.layoutInsets);
   }
 }
 
-class GestureWidgetDelegateFromState extends SingleChildLayoutDelegate {
-  GestureWidgetDelegateFromState(
-    this.state, {
-    this.imageFit,
-    this.imageHeight,
-    this.imageWidth,
-    this.rect,
-    this.copy = false,
+class RenderGestureWidgetLayout extends RenderProxyBox {
+  RenderGestureWidgetLayout({
+    BoxFit fit = BoxFit.contain,
+    GestureDetails? gestureDetails,
+    EdgeInsets layoutInsets = EdgeInsets.zero,
+    RenderBox? child,
+  }) : _fit = fit,
+       _gestureDetails = gestureDetails,
+       _layoutInsets = layoutInsets,
+       super(child);
+
+  BoxFit _fit;
+  set fit(BoxFit newValue) {
+    if (_fit == newValue) {
+      return;
+    }
+    _fit = newValue;
+    markNeedsLayout();
+  }
+
+  GestureDetails? _gestureDetails;
+  set gestureDetails(GestureDetails? newValue) {
+    if (_gestureDetails == newValue) {
+      return;
+    }
+    _gestureDetails = newValue;
+    markNeedsLayout();
+  }
+
+  EdgeInsets _layoutInsets;
+  set layoutInsets(EdgeInsets newValue) {
+    if (_layoutInsets == newValue) {
+      return;
+    }
+    _layoutInsets = newValue;
+    markNeedsLayout();
+  }
+
+  // TODO(ianh): The intrinsic dimensions of this box are wrong.
+
+  @override
+  @protected
+  Size computeDryLayout(BoxConstraints constraints) {
+    if (child != null) {
+      final Size childSize = child!.getDryLayout(constraints);
+
+      switch (_fit) {
+        case BoxFit.scaleDown:
+          final BoxConstraints sizeConstraints = constraints.loosen();
+          final Size unconstrainedSize = sizeConstraints.constrainSizeAndAttemptToPreserveAspectRatio(childSize);
+          return constraints.constrain(unconstrainedSize);
+        case BoxFit.contain:
+        case BoxFit.cover:
+        case BoxFit.fill:
+        case BoxFit.fitHeight:
+        case BoxFit.fitWidth:
+        case BoxFit.none:
+          return constraints.constrainSizeAndAttemptToPreserveAspectRatio(childSize);
+      }
+    } else {
+      return constraints.smallest;
+    }
+  }
+
+  @override
+  void performLayout() {
+    if (child != null) {
+      child!.layout(constraints.deflate(_layoutInsets), parentUsesSize: true);
+      final Rect rect = _layoutInsets.deflateRect(Offset.zero & constraints.biggest);
+      Rect destinationRect = getDestinationRect(
+        rect: rect,
+        inputSize: child!.size,
+        fit: _fit
+      );
+
+      destinationRect = _gestureDetails?.calculateFinalDestinationRect(rect, destinationRect) ?? destinationRect;
+
+      print('$rect -> $destinationRect');
+      
+      switch (_fit) {
+        case BoxFit.scaleDown:
+          final BoxConstraints sizeConstraints = constraints.loosen();
+          final Size unconstrainedSize = sizeConstraints.constrainSizeAndAttemptToPreserveAspectRatio(child!.size);
+          size = constraints.constrain(unconstrainedSize);
+        case BoxFit.contain:
+        case BoxFit.cover:
+        case BoxFit.fill:
+        case BoxFit.fitHeight:
+        case BoxFit.fitWidth:
+        case BoxFit.none:
+          size = constraints.constrainSizeAndAttemptToPreserveAspectRatio(child!.size);
+      }
+      _clearPaintData();
+    } else {
+      size = constraints.smallest;
+    }
+  }
+
+  bool? _hasVisualOverflow;
+  Matrix4? _transform;
+
+  /// {@macro flutter.material.Material.clipBehavior}
+  ///
+  /// Defaults to [Clip.none].
+  Clip get clipBehavior => _clipBehavior;
+  Clip _clipBehavior = Clip.none;
+  set clipBehavior(Clip value) {
+    if (value != _clipBehavior) {
+      _clipBehavior = value;
+      markNeedsPaint();
+      markNeedsSemanticsUpdate();
+    }
+  }
+
+  void _clearPaintData() {
+    _hasVisualOverflow = null;
+    _transform = null;
+  }
+
+  void _updatePaintData() {
+    if (_transform != null) {
+      return;
+    }
+
+    if (child == null) {
+      _hasVisualOverflow = false;
+      _transform = Matrix4.identity();
+    } else {
+      final Size childSize = child!.size;
+      final Rect rect = _layoutInsets.deflateRect(Offset.zero & size);
+      Rect destinationRect = getDestinationRect(
+        rect: rect,
+        inputSize: childSize,
+        fit: _fit
+      );
+      destinationRect = _gestureDetails?.calculateFinalDestinationRect(rect, destinationRect) ?? destinationRect;
+      final Rect sourceRect = Offset.zero & childSize;
+      _hasVisualOverflow = sourceRect.width < childSize.width || sourceRect.height < childSize.height;
+      final double scaleX = destinationRect.width / sourceRect.width;
+      final double scaleY = destinationRect.height / sourceRect.height;
+      print(destinationRect);
+      assert(scaleX.isFinite && scaleY.isFinite);
+      _transform = Matrix4.translationValues(destinationRect.left, destinationRect.top, 0.0)
+        ..scale(scaleX, scaleY, 1.0)
+        ..translate(-sourceRect.left, -sourceRect.top);
+      assert(_transform!.storage.every((double value) => value.isFinite));
+    }
+  }
+
+  TransformLayer? _paintChildWithTransform(PaintingContext context, Offset offset) {
+    final Offset? childOffset = MatrixUtils.getAsTranslation(_transform!);
+    if (childOffset == null) {
+      return context.pushTransform(
+        needsCompositing,
+        offset,
+        _transform!,
+        super.paint,
+        oldLayer: layer is TransformLayer ? layer! as TransformLayer : null,
+      );
+    } else {
+      super.paint(context, offset + childOffset);
+    }
+    return null;
+  }
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    if (child == null || size.isEmpty || child!.size.isEmpty) {
+      return;
+    }
+    _updatePaintData();
+    assert(child != null);
+    if (_hasVisualOverflow! && clipBehavior != Clip.none) {
+      layer = context.pushClipRect(
+        needsCompositing,
+        offset,
+        Offset.zero & size,
+        _paintChildWithTransform,
+        oldLayer: layer is ClipRectLayer ? layer! as ClipRectLayer : null,
+        clipBehavior: clipBehavior,
+      );
+    } else {
+      layer = _paintChildWithTransform(context, offset);
+    }
+  }
+
+  @override
+  bool hitTestChildren(BoxHitTestResult result, { required Offset position }) {
+    if (size.isEmpty || (child?.size.isEmpty ?? false)) {
+      return false;
+    }
+    _updatePaintData();
+    return result.addWithPaintTransform(
+      transform: _transform,
+      position: position,
+      hitTest: (BoxHitTestResult result, Offset position) {
+        return super.hitTestChildren(result, position: position);
+      },
+    );
+  }
+
+  @override
+  bool paintsChild(RenderBox child) {
+    assert(child.parent == this);
+    return !size.isEmpty && !child.size.isEmpty;
+  }
+
+  @override
+  void applyPaintTransform(RenderBox child, Matrix4 transform) {
+    if (!paintsChild(child)) {
+      transform.setZero();
+    } else {
+      _updatePaintData();
+      transform.multiply(_transform!);
+    }
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(EnumProperty<BoxFit>('_fit', _fit));
+    properties.add(DiagnosticsProperty<GestureDetails?>('_gestureDetails', _gestureDetails));
+    properties.add(DiagnosticsProperty<EdgeInsets>('_layoutInsets', _layoutInsets));
+  }
+}
+
+class GestureWidgetLayout extends SingleChildRenderObjectWidget {
+  const GestureWidgetLayout({
+    super.key,
+    this.fit = BoxFit.contain,
+    this.gestureDetails,
+    this.layoutInsets = EdgeInsets.zero,
+    super.child,
   });
 
-  final ExtendedImageGestureState state;
-  final double? imageWidth;
-  final double? imageHeight;
-  final BoxFit? imageFit;
-  final Rect? rect;
-  final bool copy;
+  final BoxFit fit;
+  final GestureDetails? gestureDetails;
+  final EdgeInsets layoutInsets;
 
-  Rect? destinationRect;
-
-  Rect _getDestinationRect(Rect rect) {
-    return destinationRect ??= GestureWidgetDelegateFromState.getRectFormState(
-      rect,
-      state,
-      width: imageWidth,
-      height: imageHeight,
-      fit: imageFit,
-      copy: copy,
+  @override
+  RenderGestureWidgetLayout createRenderObject(BuildContext context) {
+    return RenderGestureWidgetLayout(
+      fit: fit,
+      gestureDetails: gestureDetails,
+      layoutInsets: layoutInsets
     );
   }
 
   @override
-  Offset getPositionForChild(Size size, Size childSize) {
-    return _getDestinationRect(rect ?? (Offset.zero & size)).topLeft;
+  void updateRenderObject(BuildContext context, RenderGestureWidgetLayout renderObject) {
+    renderObject
+      ..fit = fit
+      ..gestureDetails = gestureDetails
+      ..layoutInsets = layoutInsets;
   }
 
   @override
-  bool shouldRelayout(GestureWidgetDelegateFromState oldDelegate) {
-    return destinationRect != oldDelegate.destinationRect ||
-        imageWidth != oldDelegate.imageWidth ||
-        imageHeight != oldDelegate.imageHeight ||
-        imageFit != oldDelegate.imageFit ||
-        rect != oldDelegate.rect ||
-        copy != oldDelegate.copy;
-  }
-
-  @override
-  BoxConstraints getConstraintsForChild(BoxConstraints constraints) {
-    return BoxConstraints.tight(
-      _getDestinationRect(rect ?? Offset.zero & constraints.biggest).size,
-    );
-  }
-
-  @override
-  Size getSize(BoxConstraints constraints) {
-    destinationRect = _getDestinationRect(
-      rect ?? Offset.zero & constraints.biggest,
-    );
-    return super.getSize(constraints);
-  }
-
-  static Rect getRectFormState(
-    Rect rect,
-    ExtendedImageGestureState state, {
-    double? width,
-    double? height,
-    BoxFit? fit,
-    bool copy = false,
-  }) {
-    final GestureDetails? gestureDetails = state.gestureDetails;
-
-    Rect destinationRect = getDestinationRect(
-      rect: rect,
-      inputSize: Size(
-        width ?? state.width.toDouble(),
-        height ?? state.height.toDouble(),
-      ),
-      fit: fit ?? state.fit,
-    );
-
-    if (gestureDetails != null) {
-      GestureDetails gd = gestureDetails;
-      if (copy) {
-        gd = gestureDetails.copy();
-      }
-      destinationRect = gd.calculateFinalDestinationRect(rect, destinationRect);
-    }
-    return destinationRect;
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(EnumProperty<BoxFit>('fit', fit));
+    properties.add(DiagnosticsProperty<GestureDetails?>('gestureDetails', gestureDetails));
+    properties.add(DiagnosticsProperty<EdgeInsets>('layoutInsets', layoutInsets));
   }
 }
 
@@ -761,7 +910,7 @@ class GestureWidgetDelegateFromRect extends SingleChildLayoutDelegate {
   }
 
   @override
-  bool shouldRelayout(GestureWidgetDelegateFromState oldDelegate) {
+  bool shouldRelayout(GestureWidgetDelegateFromRect oldDelegate) {
     return destinationRect != oldDelegate.destinationRect;
   }
 
